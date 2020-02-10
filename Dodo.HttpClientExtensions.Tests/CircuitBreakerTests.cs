6082d0efed47fe49dfd84a7ef2368c5ca1d92836
Mikhail Kumachev <mikhail.kumachev@gmail.com>
2020-02-10 19:24:28 +0300

    Fix CircuitBreaker tests

--- Dodo.HttpClientExtensions.Tests/CircuitBreakerTests.cs
+++ Dodo.HttpClientExtensions.Tests/CircuitBreakerTests.cs
@@ -13,33 +13,31 @@ namespace Dodo.HttpClientExtensions.Tests
 		[Test]
 		public void Should_break_after_4_concurrent_calls()
 		{
-			const int minimumThroughput = 4;
+			const int minimumThroughput = 2;
+			var retrySettings = new ExponentialRetrySettings(
+				retryCount: 5,
+				sleepDurationProvider: i => TimeSpan.FromMilliseconds(50));
 			var wrapper = Create.HttpClientWrapperWrapperBuilder
 				.WithStatusCode(HttpStatusCode.ServiceUnavailable)
 				.WithTotalTimeout(TimeSpan.FromSeconds(5))
 				.WithCircuitBreakerSettings(BuildCircuitBreakerSettings(minimumThroughput))
-				.WithRetrySettings(BuildRetrySettings())
+				.WithRetrySettings(retrySettings)
 				.Please();
 
-			Assert.CatchAsync<BrokenCircuitException>(async () => await Helper.InvokeMultipleHttpRequests(wrapper.Client, taskCount: 4));
+			const int taskCount = 4;
+			Assert.CatchAsync<BrokenCircuitException>(async () =>
+				await Helper.InvokeMultipleHttpRequests(wrapper.Client, taskCount));
 
 			Assert.AreEqual(minimumThroughput, wrapper.NumberOfCalls);
-		}
-
-		private static IRetrySettings BuildRetrySettings()
-		{
-			return new ExponentialRetrySettings(
-				retryCount: 5,
-				sleepDurationProvider: i => TimeSpan.FromMilliseconds(1));
 		}
 
 		private static ICircuitBreakerSettings BuildCircuitBreakerSettings(int throughput)
 		{
 			return new CircuitBreakerSettings(
-					failureThreshold: 0.5,
-					minimumThroughput: throughput,
-					durationOfBreak: TimeSpan.FromMinutes(1),
-					samplingDuration: TimeSpan.FromMilliseconds(20));
+				failureThreshold: 0.5,
+				minimumThroughput: throughput,
+				durationOfBreak: TimeSpan.FromMinutes(1),
+				samplingDuration: TimeSpan.FromMilliseconds(20), (_, __) => { }, () => { }, () => { Console.WriteLine("HO");});
 		}
 	}
 }
