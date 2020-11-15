902129765a969f07bad67f743144ebddb5371c6f
Mikhail Kumachev <mikhail.kumachev@gmail.com>
2020-11-15 23:34:14 +0300

    fix: Dispose HttpRequestMessage object according to LGTM alert

--- src/Dodo.HttpClient.ResiliencePolicies.Tests/Helper.cs
+++ src/Dodo.HttpClient.ResiliencePolicies.Tests/Helper.cs
@@ -12,7 +12,7 @@ namespace Dodo.HttpClient.ResiliencePolicies.Tests
 			var tasks = new Task[taskCount];
 			for (var i = 0; i < taskCount; i++)
 			{
-				var requestMessage = new HttpRequestMessage(HttpMethod.Get, uri);
+				using var requestMessage = new HttpRequestMessage(HttpMethod.Get, uri);
 				requestMessage.Headers.Add("TaskId", i.ToString());
 				tasks[i] = client.SendAsync(requestMessage);
 			}
