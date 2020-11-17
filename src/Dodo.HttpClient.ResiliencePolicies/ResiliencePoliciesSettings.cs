2912bdcac222a653526a53de3c6017e367f1253c
Mikhail Kumachev <mikhail.kumachev@gmail.com>
2020-11-17 22:30:09 +0300

    feat: Add policies actions properties forwarding to the root settings

--- src/Dodo.HttpClient.ResiliencePolicies/ResiliencePoliciesSettings.cs
+++ src/Dodo.HttpClient.ResiliencePolicies/ResiliencePoliciesSettings.cs
@@ -1,20 +1,14 @@
 using System;
+using System.Net.Http;
 using Dodo.HttpClientResiliencePolicies.CircuitBreakerPolicy;
 using Dodo.HttpClientResiliencePolicies.RetryPolicy;
 using Dodo.HttpClientResiliencePolicies.TimeoutPolicy;
+using Polly;
 
 namespace Dodo.HttpClientResiliencePolicies
 {
 	public class ResiliencePoliciesSettings
 	{
-		public ITimeoutPolicySettings OverallTimeoutPolicySettings { get; set; }
-
-		public ITimeoutPolicySettings TimeoutPerTryPolicySettings { get; set; }
-
-		public IRetryPolicySettings RetryPolicySettings { get; set; }
-
-		public ICircuitBreakerPolicySettings CircuitBreakerPolicySettings { get; set; }
-
 		public ResiliencePoliciesSettings()
 		{
 			OverallTimeoutPolicySettings = new OverallTimeoutPolicySettings();
@@ -33,6 +27,55 @@ namespace Dodo.HttpClientResiliencePolicies
 			TimeoutPerTryPolicySettings = new TimeoutPerTryPolicySettings(timeoutPerTry);
 			RetryPolicySettings = retryPolicyPolicySettings;
 			CircuitBreakerPolicySettings = circuitBreakerPolicyPolicySettings;
+		}
+
+		public ITimeoutPolicySettings OverallTimeoutPolicySettings { get; set; }
+		public ITimeoutPolicySettings TimeoutPerTryPolicySettings { get; set; }
+		public IRetryPolicySettings RetryPolicySettings { get; set; }
+		public ICircuitBreakerPolicySettings CircuitBreakerPolicySettings { get; set; }
+
+		public Action<DelegateResult<HttpResponseMessage>, TimeSpan> OnRetry
+		{
+			get => RetryPolicySettings?.OnRetry;
+			set
+			{
+				if (RetryPolicySettings == null) throw new NullReferenceException(
+					$"{nameof(RetryPolicySettings)} should be initialized first.");
+				RetryPolicySettings.OnRetry = value;
+			}
+		}
+
+		public Action<DelegateResult<HttpResponseMessage>, TimeSpan> OnBreak
+		{
+			get => CircuitBreakerPolicySettings?.OnBreak;
+			set
+			{
+				if (CircuitBreakerPolicySettings == null) throw new NullReferenceException(
+					$"{nameof(CircuitBreakerPolicySettings)} should be initialized first.");
+				CircuitBreakerPolicySettings.OnBreak = value;
+			}
+		}
+
+		public Action OnReset
+		{
+			get => CircuitBreakerPolicySettings?.OnReset;
+			set
+			{
+				if (CircuitBreakerPolicySettings == null) throw new NullReferenceException(
+					$"{nameof(CircuitBreakerPolicySettings)} should be initialized first.");
+				CircuitBreakerPolicySettings.OnReset = value;
+			}
+		}
+
+		public Action OnHalfOpen
+		{
+			get => CircuitBreakerPolicySettings?.OnHalfOpen;
+			set
+			{
+				if (CircuitBreakerPolicySettings == null) throw new NullReferenceException(
+					$"{nameof(CircuitBreakerPolicySettings)} should be initialized first.");
+				CircuitBreakerPolicySettings.OnHalfOpen = value;
+			}
 		}
 	}
 }
